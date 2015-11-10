bb97850e7a6a206108b0dce1f49b9c63eb79afab
Christopher Lee <yensen@gmail.com>
2015-11-10 09:07:45 -0800

    experiment with chart

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var photoCollect = [];
+var picture1 = document.createElement('img');
+var picture2 = document.createElement('img');
+var temp = document.getElementById('temp');
 
 function Photo (site, path) {
   this.site = site;
@@ -6,7 +9,6 @@ function Photo (site, path) {
   this.votes = 0;
   photoCollect.push(this);
 }
-
 // returns a random index
 calcRandom = function () {
     return Math.floor(Math.random() * photoCollect.length);
@@ -18,40 +20,33 @@ calcNewIndex = function (index1) {
   } while (index1 === index2)
   return index2;
 }
+// increments vote of the object with the index passed to it
 vote = function (index) {
   console.log(photoCollect[index].site + ' had ' + photoCollect[index].votes + ' votes.');
   photoCollect[index].votes++;
   console.log(photoCollect[index].site + ' now has ' + photoCollect[index].votes + ' votes.');
+  // these two lines are necessary to prevent previous event listeners from voting again
   picture1.parentNode.removeChild(picture1);
   picture2.parentNode.removeChild(picture2);
+
   displayPhotos();
 }
-  var picture1 = document.getElementById('img1');
-  var picture2 = document.getElementById('img2');
-
+// creates image elements, sets source, event listeners
 displayPhotos = function () {
-
   var index1 = calcRandom();
   var index2 = calcNewIndex(index1);
-  var temp = document.getElementById('temp');
-  // var picture1 = document.getElementById('img1');
-  // var picture2 = document.getElementById('img2');
 
-  // picture1.parentNode.removeChild(picture1);
-  // picture2.parentNode.removeChild(picture2);
+  picture1 = document.createElement('img');
+  picture2 = document.createElement('img');
 
-   picture1 = document.createElement('img');
-   picture2 = document.createElement('img');
-   picture1.setAttribute('id', 'img1');
-   picture2.setAttribute('id', 'img2');
-   picture1.setAttribute('src', photoCollect[index1].path);
-   picture1.setAttribute('width', '300');
-   picture2.setAttribute('src', photoCollect[index2].path);
-   picture2.setAttribute('width', '300');
-   temp.appendChild(picture1);
-   temp.appendChild(picture2);
-   // picture1 = document.getElementById('img1');
-   // picture2 = document.getElementById('img2');
+  picture1.setAttribute('src', photoCollect[index1].path);
+  picture1.setAttribute('width', '300');
+
+  picture2.setAttribute('src', photoCollect[index2].path);
+  picture2.setAttribute('width', '300');
+
+  temp.appendChild(picture1);
+  temp.appendChild(picture2);
 
   picture1.addEventListener('click', function(){vote(index1)});
   picture2.addEventListener('click', function(){vote(index2)});
@@ -71,8 +66,18 @@ var wikipedia = new Photo('Wikipedia', 'img/wikipedia.png');
 var youtube = new Photo('YouTube', 'img/youtube.png');
 
 displayPhotos();
-// var next = document.getElementById('compare');
-// next.addEventListener('click', displayPhotos);
 
-
-
+var resultsData = {
+  labels : ["Amazon","Dropbox","eBay","Facebook","GitHub","Google","LinkedIn","Netflix","Reddit","Twitter","Wikipedia","YouTube"],
+  datasets : [
+    {
+      fillColor : "rgba(172,194,132,0.4)",
+      strokeColor : "#ACC26D",
+      pointColor : "#fff",
+      pointStrokeColor : "#9DB86D",
+      data : [203,156,99,251,305,247,203,156,99,251,305,247]
+    }
+  ]
+}
+var results = document.getElementById('results').getContext('2d');
+new Chart(results).Line(resultsData);
