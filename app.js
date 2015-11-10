d59d2179ef8270fa54556ee9b57c303f8348277a
Christopher Lee <yensen@gmail.com>
2015-11-10 15:39:24 -0800

    included boxes to hold l/r images,captions, added basic css styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var photoAlbum = [];
+var photoCollect = [];
 var amazon = new Photo('Amazon', 'img/amazon.png');
 var dropbox = new Photo('Dropbox', 'img/dropbox.png');
 var ebay = new Photo('eBay', 'img/eBay.png');
@@ -11,21 +11,22 @@ var reddit = new Photo('Reddit', 'img/reddit.png');
 var twitter = new Photo('Twitter', 'img/twitter.png');
 var wikipedia = new Photo('Wikipedia', 'img/wikipedia.png');
 var youtube = new Photo('YouTube', 'img/youtube.png');
-var photoTracker = new Tracker(photoAlbum);
+var photoTracker = new Tracker(photoCollect);
 
 function Photo (site, path) {
   this.site = site;
   this.path = path;
   this.votes = 0;
-  photoAlbum.push(this);
+  photoCollect.push(this);
 }
 
-function Tracker (photos) {
-  this.photoCollect = photos;
-  this.temp = document.getElementById('temp');
+function Tracker (photoCollect) {
+  this.photoCollect = photoCollect;
+  this.box1 = document.getElementById('box1');
+  this.box2 = document.getElementById('box2');
 
   this.calcRandom = function () {
-      return Math.floor(Math.random() * this.photoCollect.length);
+    return Math.floor(Math.random() * this.photoCollect.length);
   }
 
   this.calcNewIndex = function (index1) {
@@ -42,24 +43,43 @@ function Tracker (photos) {
     this.picture1 = document.createElement('img');
     this.picture2 = document.createElement('img');
 
+    this.caption1 = document.createElement('h1');
+    this.caption2 = document.createElement('h1');
+
+    this.caption1.setAttribute('id','caption1');
+    this.caption2.setAttribute('id','caption2');
+
+    this.caption1.textContent = 'Click to vote for ' + this.photoCollect[index1].site;
+    this.caption2.textContent = 'Click to vote for ' + this.photoCollect[index2].site;
+
+    this.picture1.setAttribute('id','img1');
     this.picture1.setAttribute('src', this.photoCollect[index1].path);
-    this.picture1.setAttribute('width', '300');
 
+    this.picture2.setAttribute('id','img2');
     this.picture2.setAttribute('src', this.photoCollect[index2].path);
-    this.picture2.setAttribute('width', '300');
 
-    this.temp.appendChild(this.picture1);
-    this.temp.appendChild(this.picture2);
+    this.box1.appendChild(this.caption1);
+    this.box1.appendChild(this.picture1);
+
+    this.box2.appendChild(this.caption2);
+    this.box2.appendChild(this.picture2);
 
     this.picture1.addEventListener('click', function(){photoTracker.vote(index1)});
     this.picture2.addEventListener('click', function(){photoTracker.vote(index2)});
   }
 
   this.vote = function (index) {
+    var currentVote = document.getElementById('currentVote');
+    currentVote.innerHTML = 'You voted for ' + this.photoCollect[index].site;
+    document.body.appendChild(currentVote);
+
     console.log(this.photoCollect[index].site + ' had ' + this.photoCollect[index].votes + ' votes.');
     this.photoCollect[index].votes++;
     console.log(this.photoCollect[index].site + ' now has ' + this.photoCollect[index].votes + ' votes.');
-    this.temp.innerHTML = null;
+
+    this.box1.innerHTML = null;
+    this.box2.innerHTML = null;
+
     this.displayPhotos();
   }
 }
