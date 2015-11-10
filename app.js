3f8e262a4b35184834ae57a9b67ac3bd4210607f
Christopher Lee <yensen@gmail.com>
2015-11-10 13:46:26 -0800

    created tracker object, made all functions into tracker methods

--- app.js
+++ app.js
@@ -1,57 +1,4 @@
-var photoCollect = [];
-var picture1 = document.createElement('img');
-var picture2 = document.createElement('img');
-var temp = document.getElementById('temp');
-
-function Photo (site, path) {
-  this.site = site;
-  this.path = path;
-  this.votes = 0;
-  photoCollect.push(this);
-}
-// returns a random index
-calcRandom = function () {
-    return Math.floor(Math.random() * photoCollect.length);
-}
-// makes sure two indices are different
-calcNewIndex = function (index1) {
-  do {
-    var index2 = calcRandom();
-  } while (index1 === index2)
-  return index2;
-}
-// increments vote of the object with the index passed to it
-vote = function (index) {
-  console.log(photoCollect[index].site + ' had ' + photoCollect[index].votes + ' votes.');
-  photoCollect[index].votes++;
-  console.log(photoCollect[index].site + ' now has ' + photoCollect[index].votes + ' votes.');
-  // these two lines are necessary to prevent previous event listeners from voting again
-  picture1.parentNode.removeChild(picture1);
-  picture2.parentNode.removeChild(picture2);
-
-  displayPhotos();
-}
-// creates image elements, sets source, event listeners
-displayPhotos = function () {
-  var index1 = calcRandom();
-  var index2 = calcNewIndex(index1);
-
-  picture1 = document.createElement('img');
-  picture2 = document.createElement('img');
-
-  picture1.setAttribute('src', photoCollect[index1].path);
-  picture1.setAttribute('width', '300');
-
-  picture2.setAttribute('src', photoCollect[index2].path);
-  picture2.setAttribute('width', '300');
-
-  temp.appendChild(picture1);
-  temp.appendChild(picture2);
-
-  picture1.addEventListener('click', function(){vote(index1)});
-  picture2.addEventListener('click', function(){vote(index2)});
-}
-
+var photoAlbum = [];
 var amazon = new Photo('Amazon', 'img/amazon.png');
 var dropbox = new Photo('Dropbox', 'img/dropbox.png');
 var ebay = new Photo('eBay', 'img/eBay.png');
@@ -64,20 +11,72 @@ var reddit = new Photo('Reddit', 'img/reddit.png');
 var twitter = new Photo('Twitter', 'img/twitter.png');
 var wikipedia = new Photo('Wikipedia', 'img/wikipedia.png');
 var youtube = new Photo('YouTube', 'img/youtube.png');
+var photoTracker = new Tracker(photoAlbum);
 
-displayPhotos();
-
-var resultsData = {
-  labels : ["Amazon","Dropbox","eBay","Facebook","GitHub","Google","LinkedIn","Netflix","Reddit","Twitter","Wikipedia","YouTube"],
-  datasets : [
-    {
-      fillColor : "rgba(172,194,132,0.4)",
-      strokeColor : "#ACC26D",
-      pointColor : "#fff",
-      pointStrokeColor : "#9DB86D",
-      data : [203,156,99,251,305,247,203,156,99,251,305,247]
-    }
-  ]
+function Photo (site, path) {
+  this.site = site;
+  this.path = path;
+  this.votes = 0;
+  photoAlbum.push(this);
 }
-var results = document.getElementById('results').getContext('2d');
-new Chart(results).Line(resultsData);
+
+function Tracker (photos) {
+  this.photoCollect = photos;
+  this.temp = document.getElementById('temp');
+
+  this.calcRandom = function () {
+      return Math.floor(Math.random() * this.photoCollect.length);
+  }
+
+  this.calcNewIndex = function (index1) {
+    do {
+      var index2 = this.calcRandom();
+    } while (index1 === index2)
+    return index2;
+  }
+
+  this.displayPhotos = function () {
+    var index1 = this.calcRandom();
+    var index2 = this.calcNewIndex(index1);
+
+    this.picture1 = document.createElement('img');
+    this.picture2 = document.createElement('img');
+
+    this.picture1.setAttribute('src', this.photoCollect[index1].path);
+    this.picture1.setAttribute('width', '300');
+
+    this.picture2.setAttribute('src', this.photoCollect[index2].path);
+    this.picture2.setAttribute('width', '300');
+
+    this.temp.appendChild(this.picture1);
+    this.temp.appendChild(this.picture2);
+
+    this.picture1.addEventListener('click', function(){photoTracker.vote(index1)});
+    this.picture2.addEventListener('click', function(){photoTracker.vote(index2)});
+  }
+
+  this.vote = function (index) {
+    console.log(this.photoCollect[index].site + ' had ' + this.photoCollect[index].votes + ' votes.');
+    this.photoCollect[index].votes++;
+    console.log(this.photoCollect[index].site + ' now has ' + this.photoCollect[index].votes + ' votes.');
+    this.temp.innerHTML = null;
+    this.displayPhotos();
+  }
+}
+
+photoTracker.displayPhotos();
+
+// var resultsData = {
+//   labels : ["Amazon","Dropbox","eBay","Facebook","GitHub","Google","LinkedIn","Netflix","Reddit","Twitter","Wikipedia","YouTube"],
+//   datasets : [
+//     {
+//       fillColor : "rgba(172,194,132,0.4)",
+//       strokeColor : "#ACC26D",
+//       pointColor : "#fff",
+//       pointStrokeColor : "#9DB86D",
+//       data : [203,156,99,251,305,247,203,156,99,251,305,247]
+//     }
+//   ]
+// }
+// var results = document.getElementById('results').getContext('2d');
+// new Chart(results).Line(resultsData);
