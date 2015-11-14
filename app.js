32d8ddbb29b8c65b0f0d640cc5b9f7a9be0fcc9a
Christopher Lee <yensen@gmail.com>
2015-11-14 15:36:49 -0800

    fixed bug that displays previous charts on mouseover by resetting canvas each time

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 var album = [];
 var data = [];
-if (localStorage.data) {
-  data = JSON.parse(localStorage.getItem('data'));
-  album = JSON.parse(localStorage.getItem('album'));
-}
-var skillsChart;
 var sites = ['Amazon', 'Dropbox', 'eBay', 'Evernote','Facebook', 'GitHub', 'Google', 'Instagram','LinkedIn', 'Netflix', 'Reddit', 'Twitter', 'Wikipedia', 'YouTube'];
-var colors = ['rgb(253,134,9)','rgb(13,102,223)','rgb(117,174,19)','rgb(42,181,78)','rgb(45,68,134)','rgb(17,16,15)','rgb(226,44,41)','rgb(141,91,68)','rgb(13,98,166)','rgb(167,10,28)','rgb(253,44,8)','rgb(46,191,255)','rgb(0,0,0)','rgb(215,11,29)'];
+var colors = [[253,134,9],[13,102,223],[117,174,19],[42,181,78],[45,68,134],[17,16,15],[226,44,41],[141,91,68],[13,98,166],[167,10,28],[253,44,8],[46,191,255],[0,0,0],[215,11,29]];
+
+var skillsChart;    // declare chart as a global object
 var context = document.getElementById('results').getContext('2d');
 
 // Photo Constructor
-function Photo (site, path, siteColor) {
+function Photo (site, path, siteColor, siteHighlight) {
   this.site = site;
   this.path = path;
   this.siteColor = siteColor;
@@ -20,7 +17,7 @@ function Photo (site, path, siteColor) {
     value: 0,
     label: site,
     color: siteColor,
-    highlight: 'rgba(253,134,9,0.5)'
+    highlight: siteHighlight
   });
 }
 
@@ -34,29 +31,28 @@ var tracker = {
   createAlbum: function () {
     for (var i = 0; i < sites.length; i++) {
       var path = 'img/' + sites[i] + '.png';
-      var siteColor = colors[i];
-      new Photo(sites[i], path, siteColor);
-      console.log('Photo added!');
+      var siteColor = 'rgb(' + colors[i][0] + ',' + colors[i][1] + ',' + colors[i][2] + ')';
+      var siteHighlight = 'rgba(' + colors[i][0] + ',' + colors[i][1] + ',' + colors[i][2] + ',0.5)';
+      new Photo(sites[i], path, siteColor, siteHighlight);
     }
     localStorage.setItem('album', JSON.stringify(album));
+    localStorage.setItem('data', JSON.stringify(data));
   },
-
   calcRandom: function () {
     return Math.floor(Math.random() * album.length);
   },
-
   calcNewIndex: function (index1) {
     do {
       var index2 = this.calcRandom();
     } while (index1 === index2)
     return index2;
   },
-
   displayPhotos: function () {
-
+    // calculate two unique random indices
     var index1 = this.calcRandom();
     var index2 = this.calcNewIndex(index1);
 
+    // populate box1 with new random image/caption with event listener to vote
     this.picture1 = document.createElement('img');
     this.picture1.setAttribute('src', album[index1].path);
     this.picture1.addEventListener('click', function(){tracker.vote(index1)});
@@ -68,6 +64,7 @@ var tracker = {
     this.box1.appendChild(this.caption1);
     this.box1.appendChild(this.picture1);
 
+    // populate box2 with new random image/caption with event listener to vote
     this.picture2 = document.createElement('img');
     this.picture2.setAttribute('src', album[index2].path);
     this.picture2.addEventListener('click', function(){tracker.vote(index2)});
@@ -79,10 +76,13 @@ var tracker = {
     this.box2.appendChild(this.caption2);
     this.box2.appendChild(this.picture2);
 
+    // if local storage exists, retrieve it and display existing chart/msg
     if (localStorage.msg) {
       var msg = JSON.parse(localStorage.getItem('msg'));
       var color = JSON.parse(localStorage.getItem('color'));
       var result = document.getElementById('result');
+      album = JSON.parse(localStorage.getItem('album'));
+      data = JSON.parse(localStorage.getItem('data'));
       skillsChart = new Chart(context).Pie(data, {
           animationSteps : 45,
           segmentShowStroke : false,
@@ -95,9 +95,7 @@ var tracker = {
       this.box3.appendChild(result);
     }
   },
-
   vote: function (index) {
-
     album[index].votes++;
     data[index].value = album[index].votes;
 
@@ -105,6 +103,7 @@ var tracker = {
     currentVote.innerHTML = 'You voted for ' + album[index].site;
     currentVote.style.color = album[index].siteColor;
     this.box3.appendChild(currentVote);
+
     this.updateChart(index);
     this.showWinner();
     this.box1.innerHTML = null;
@@ -113,49 +112,50 @@ var tracker = {
   },
 
   updateChart: function (index) {
-
-    if (this.firstVote && !localStorage.data) {
-      // context = document.getElementById('results').getContext('2d');
-      skillsChart = new Chart(context).Pie(data, {
-          animationSteps : 45,
-          segmentShowStroke : false,
-          animationEasing : "easeOutBounce",
-          scaleShowLabelBackdrop : true
-      });
-      this.firstVote = false;
-    }
-    else {
-      skillsChart.segments[index].value = album[index].votes;
-      skillsChart.update();
-    }
+    // RESET THE CANVAS
+    var context = document.getElementById('results');
+    this.box3.removeChild(context);
+    var context = document.createElement('canvas');
+    context.setAttribute ('id', 'results');
+    context.setAttribute ('width', '375');
+    context.setAttribute ('height', '250');
+    this.box3.insertBefore(context, this.box3.firstChild.nextSibling);
+    // CREATE/UPDATE THE CHART
+    context = document.getElementById('results').getContext('2d');
+    skillsChart = new Chart(context).Pie(data, {
+        animationSteps : 45,
+        segmentShowStroke : false,
+        animationEasing : "easeOutBounce",
+        scaleShowLabelBackdrop : true
+    });
+    skillsChart.segments[index].value = album[index].votes;
+    skillsChart.update();
   },
   showWinner: function () {
-
-    var count = [];
-    var msg;
+    var count = []; //stores number of votes
+    var msg;  // stores customized message summary for the user
     var result = document.getElementById('result');
 
-    for (var i = 0; i < album.length; i++) {
-      count[i] = album[i].votes;
-    }
+    for (var i = 0; i < album.length; i++) {count[i] = album[i].votes;}
     var max = Math.max.apply(null, count);
-    var temp = album.filter(function(itm){return itm.votes === max;});
+    // leaders is an array of photo objects, which have the most votes
+    var leaders = album.filter(function(itm){return itm.votes === max;});
 
-    if (temp.length === 1) {msg = temp[0].site + ' is in the lead with ';}
-    else if (temp.length === 2) {msg = 'There is a tie between: <br>' + temp[0].site + ' and ' + temp[1].site + '.<br>Each has '}
+    if (leaders.length === 1) {msg = leaders[0].site + ' is in the lead with ';}
+    else if (leaders.length === 2) {msg = 'There is a tie between: <br>' + leaders[0].site + ' and ' + leaders[1].site + '.<br>Each has '}
     else {
-      msg = 'There is a tie between: <br>' + temp[0].site;
-      for (var j = 1; j < temp.length; j++) {
-          if (j < temp.length - 1) {msg += ',<br>' + temp[j].site;}
-          else {msg += ', and ' + temp[j].site;}
+      msg = 'There is a tie between: <br>' + leaders[0].site;
+      for (var j = 1; j < leaders.length; j++) {
+          if (j < leaders.length - 1) {msg += ',<br>' + leaders[j].site;}
+          else {msg += ', and ' + leaders[j].site;}
       }
       msg += '.<br>Each has ';
     }
 
-    if (temp[0].votes === 1) {msg += '1 vote';}
-    else {msg += temp[0].votes + ' votes'}
+    if (leaders[0].votes === 1) {msg += '1 vote';}
+    else {msg += leaders[0].votes + ' votes'}
 
-    if (temp.length === 1) {var color = temp[0].siteColor;}
+    if (leaders.length === 1) {var color = leaders[0].siteColor;}
     else {var color = '#000';}
 
     result.innerHTML = msg;
@@ -168,14 +168,18 @@ var tracker = {
     localStorage.setItem('album', JSON.stringify(album));
     localStorage.setItem('firstVote', JSON.stringify(this.firstVote));
     localStorage.setItem('data', JSON.stringify(data));
+  },
+  reset: function () {
+    localStorage.clear();
   }
 }
 
+// if album exists in local storage, then retrieve it
 if (localStorage.album) {
-  console.log('Local storage found!');
   album = JSON.parse(localStorage.getItem('album'));
+  data = JSON.parse(localStorage.getItem('data'));
 }
-else {tracker.createAlbum();
-console.log('Local storage not found. Creating album now!');}
+// otherwise, create a new album
+else {tracker.createAlbum();}
 
 tracker.displayPhotos();
